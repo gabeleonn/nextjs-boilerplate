38f607b97bc5e32d5030369f2437c27c2f7f595d
Gabriel Leon <gabriel.leon@baritecnologia.com.br>
2021-06-01 15:33:16 -0300

    feat: change _app layout

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,12 @@
 import { GlobalStyle } from "../styles/GlobaStyles";
 
 function MyApp({ Component, pageProps }) {
-  return <><Component {...pageProps} /><GlobalStyle /></>
+  return (
+    <>
+      <Component {...pageProps} />
+      <GlobalStyle />
+    </>
+  )
 }
 
 export default MyApp
